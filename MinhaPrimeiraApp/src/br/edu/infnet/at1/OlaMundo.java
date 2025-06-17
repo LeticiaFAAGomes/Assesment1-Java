a033aaf2f603806c661409585bc74137dcda93a3
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2025-06-17 16:45:06 -0300

    AT.1 - Instalando e configurando o ambiente Java

---
+++ MinhaPrimeiraApp/src/br/edu/infnet/at1/OlaMundo.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.at1;
+
+public class OlaMundo {
+	
+	public static void main(String[] args) {
+		
+		System.out.printf("Olá, meu nome é Letícia e estou aprendendo Java!");
+	}
+}
