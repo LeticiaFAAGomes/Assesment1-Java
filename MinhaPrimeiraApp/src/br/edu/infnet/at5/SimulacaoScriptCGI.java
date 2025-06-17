12f91e7f72d90625c88e69618b05b6f77f6ac62c
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2025-06-17 16:54:09 -0300

    AT.5 - Criando um Programa CGI em Java

---
+++ MinhaPrimeiraApp/src/br/edu/infnet/at5/SimulacaoScriptCGI.java
@@ -0,0 +1,17 @@
+package br.edu.infnet.at5;
+
+public class SimulacaoScriptCGI {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Content-Type: text/html");
+		System.out.println();
+		System.out.println("<html>");
+		System.out.println("<head><title>Saudação CGI</title></head>");
+		System.out.println("<body>");
+		System.out.println("<h1>Olá, Terráqueos!</h1>");
+		System.out.println("</html>");
+		System.out.println("</body>");
+	}
+
+}
